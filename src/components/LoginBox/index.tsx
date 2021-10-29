6b2a922f98b8ed5fbe81aeaba13aa444062bdbd2
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2021-10-29 08:57:56 -0300

    sign in and out

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,11 +1,15 @@
 import styles from './styles.module.scss'
 import { VscGithubInverted } from 'react-icons/vsc'
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
 
 export function LoginBox() {
+  const { signInUrl } = useContext(AuthContext)
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Sign in and share your message</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Sign in with Github
       </a>
