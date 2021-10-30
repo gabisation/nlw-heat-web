815bc1d4a6146028efa38400987e4cde469ffbbb
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2021-10-30 20:54:38 -0300

    add websocket

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss'
 import { api } from '../../services/api'
 import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 
 type Message = {
   id: string,
@@ -11,8 +12,28 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage)
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    setInterval(() => {
+      setMessages(prevState => [
+        messagesQueue[0],
+        prevState[0],
+        prevState[1]
+      ].filter(Boolean))
+
+      messagesQueue.shift()
+    }, 3000)
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>('messages/latest').then(response => {
